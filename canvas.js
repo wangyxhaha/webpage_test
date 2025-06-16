e36abf183a4f7c0d3e701c9efe1876911257fcc0
wangyxhaha <55278006+wangyxhaha@users.noreply.github.com>
2025-06-16 10:01:57 +0800

    Update canvas.js

--- canvas.js
+++ canvas.js
@@ -142,10 +142,10 @@ class Canvas{
     touchStartCallBack(evt){
         evt.preventDefault();
         var logicalPos=logicalEvtChange(this,evt.touches[0]);
-        // if (this.clickFocusPoint!=-1){
-        //     this.touchStartCallBackArray[this.clickFocusPoint].func(logicalPos);
-        //     return;
-        // }
+        if (this.clickFocusPoint!=-1){ //因为触摸屏有多指点击，有必要限制一下有焦点时的点击
+            this.touchStartCallBackArray[this.clickFocusPoint].func(logicalPos);
+            return;
+        }
         for (var i in this.touchStartCallBackArray){
             if (this.touchStartCallBackArray[i].func(logicalPos)){
                 this.clickFocusPoint=i;
