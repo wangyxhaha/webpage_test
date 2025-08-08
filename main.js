1be67339c7bb88671d8b7eb94e83db9f0b62cb2f
LiuTianZe070119 <liutianze20070119@qq.com>
2025-08-08 14:32:15 +0800

    lsh 1.0

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ import Dialog from "./dialog.js";
 
 var canvas=new Canvas("gameCanvas",935,935)
 
-import("./byz/byz.js")
+import("./lsh/lsh.js")
     .then(module=>{
         module.default.init(canvas);
     });
