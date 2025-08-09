44ec6aa1c9fa26df22da6778deb7911f08dffb84
LiuTianZe070119 <liutianze20070119@qq.com>
2025-08-09 17:49:39 +0800

    zsz bata1

--- zsz/zsz.js
+++ zsz/zsz.js
@@ -6,9 +6,9 @@ import Canvas from "../canvas.js";
 import Button from "../button.js";
 import Animation from "../animation.js";
 import Input from "../input.js";
+import Text from "../text.js";
 import "../buttonPlugin.js";
-//import Music from "../music.js";
-//import Text from "../text.js";
+import "../spritePlugin.js";
 
 var cfg=[
     {
@@ -55,6 +55,41 @@ var cfg=[
         name: "lock",
         type: "image",
         value: "./zsz/data/锁.png"
+    },
+    {
+        name: "notice",
+        type: "image",
+        value: "./zsz/data/说明.png"
+    },
+    {
+        name: "lose",
+        type: "image",
+        value: "./zsz/data/失败.png"
+    },
+    {
+        name: "start",
+        type: "image",
+        value: "./zsz/data/start.png"
+    },
+    {
+        name: "try_again",
+        type: "image",
+        value: "./zsz/data/try again.png"
+    },
+    {
+        name: "zhoubin",
+        type: "image",
+        value: "./zsz/data/zsz周斌.png"
+    },
+    {
+        name: "hand",
+        type: "image",
+        value: "./zsz/data/zsz手.png"
+    },
+    {
+        name: "phone",
+        type: "image",
+        value: "./zsz/data/zsz手机.png"
     }
 ]
 
@@ -94,6 +129,184 @@ function build(canvas){
     });
     zsz_answer_box_fake_disable_button.setClickable(false);
     zsz_answer_box_fake_disable_button.setIgnoreClickEven(true);
+
+    var zsz_game_scene=new Animation([
+        {
+            image: res.getResource("notice"),
+            interval: Infinity
+        },
+        {
+            image: null,
+            interval: Infinity
+        },
+        {
+            image: res.getResource("lose"),
+            interval: Infinity
+        }
+    ]
+    );
+
+    //TODO zsz左场景游戏搭建
+
+    var zsz_left_scene_game_bg=new Button(canvas.scene("zsz_left_scene"),0,0,0,0,10,zsz_game_scene,null,null,()=>{},()=>{});
+
+    var zsz_left_scene_start_button=new Button(canvas.scene("zsz_left_scene"),0,0,308,106,12,res.getResource("start"),null,null,()=>{
+        zsz_game_scene.to(1);
+        zsz_left_scene_start_button.setTransparentAlpha(0);
+        zsz_left_scene_start_button.setClickable(false);
+        zsz_left_scene_start_button.setIgnoreClickEven(true);
+        console.log("Game Start");
+        document.dispatchEvent(startEvent);
+    },()=>{},305,555);
+
+    var startEvent=new CustomEvent('startGame');
+    var loseEvent=new CustomEvent('loseGame');
+
+    var zsz_left_scene_lose_button=new Button(canvas.scene("zsz_left_scene"),0,0,255,67,11,res.getResource("try_again"),null,null,()=>{
+        zsz_game_scene.to(0);
+        zsz_left_scene_lose_button.setTransparentAlpha(0);
+        zsz_left_scene_lose_button.setClickable(false);
+        zsz_left_scene_start_button.setTransparentAlpha(1);
+        zsz_left_scene_start_button.setClickable(true);
+        zsz_left_scene_start_button.setIgnoreClickEven(false);
+        
+    },()=>{},336,610);
+    zsz_left_scene_lose_button.setTransparentAlpha(0);
+    zsz_left_scene_lose_button.setClickable(false);
+
+    //TODO 显示倒计时
+
+    var count_down_text=new Text(canvas.scene("zsz_left_scene"),556,60,31);
+    count_down_text.setFillColor("Black");
+    count_down_text.setFont("黑体");
+    count_down_text.setFontHeight(40);
+    count_down_text.setTransparentAlpha(0);
+ 
+    document.addEventListener('startGame',()=>{
+        //console.log("!start");
+        zsz_left_scene_right_arrow.setTransparentAlpha(0);
+        zsz_left_scene_right_arrow.setClickable(false);
+        zsz_left_scene_phone.setTransparentAlpha(1);
+        zsz_left_scene_phone.setPostition(383,464);
+        var seconds=30; //设置倒计时时间
+        count_down_text.value=`剩余时间：${seconds} s`;
+        count_down_text.setTransparentAlpha(1);
+        const timer=setInterval(()=>{
+            count_down_text.value=`剩余时间：${seconds} s`;
+            if(seconds==0){
+                clearInterval(timer);
+                clearInterval(teacherGenerator_hard);
+                clearInterval(handGenerator_hard);
+                document.dispatchEvent(loseEvent); //仅调试用
+            }
+            if(seconds==30){
+                easyMode();
+            }
+            if(seconds==22){
+                clearInterval(teacherGenerator_easy);
+                clearInterval(handGenerator_easy);
+                mediumMode();
+            }
+            if(seconds==13){
+                clearInterval(teacherGenerator_medium);
+                clearInterval(handGenerator_medium);
+                hardMode();
+            }
+            seconds-=1;
+        },1000);
+    });
+
+    document.addEventListener('loseGame',()=>{
+        zsz_game_scene.to(2);
+        count_down_text.setTransparentAlpha(0);
+        zsz_left_scene_lose_button.setTransparentAlpha(1);
+        zsz_left_scene_lose_button.setClickable(true);
+        zsz_left_scene_phone.setTransparentAlpha(0);
+        clearInterval(teacherGenerator_easy);
+        clearInterval(teacherGenerator_medium);
+        clearInterval(teacherGenerator_hard);
+        clearInterval(handGenerator_easy);
+        clearInterval(handGenerator_medium);
+        clearInterval(handGenerator_hard);
+    })
+
+    //TODO 显示竖向周斌和横向手掌
+
+    var teachers=[];
+    var hands=[];
+
+    function getRandomNum(min,max) {
+        min=Math.ceil(min);
+        max=Math.floor(max);
+        return Math.floor(Math.random()*(max-min+1))+min;
+    }
+
+    var cnt1=0;
+    var cnt2=0;
+
+    var teacherGenerator_easy;
+    var teacherGenerator_medium;
+    var teacherGenerator_hard;
+    var handGenerator_easy;
+    var handGenerator_medium;
+    var handGenerator_hard;
+
+    function easyMode(){
+        teacherGenerator_easy=setInterval(()=>{
+            let x=getRandomNum(0,835);
+            let speed=getRandomNum(4000,5000);
+            teachers.push(new Button(canvas.scene("zsz_left_scene"),x,-200,0,0,11,res.getResource("zhoubin"),null,null,()=>{},()=>{}));
+            teachers[cnt1].moveTo(x,1000,speed);
+            cnt1++;
+        },3000);
+        handGenerator_easy=setInterval(()=>{
+            let y=getRandomNum(0,835);
+            let speed=getRandomNum(4000,5000);
+            hands.push(new Button(canvas.scene("zsz_left_scene"),-200,y,0,0,11,res.getResource("hand"),null,null,()=>{},()=>{}));
+            hands[cnt2].moveTo(1000,y,speed);
+            cnt2++;
+        },3000);
+    }
+
+    function mediumMode(){
+        teacherGenerator_medium=setInterval(()=>{
+            let x=getRandomNum(0,835);
+            let speed=getRandomNum(3000,4000);
+            teachers.push(new Button(canvas.scene("zsz_left_scene"),x,-200,0,0,11,res.getResource("zhoubin"),null,null,()=>{},()=>{}));
+            teachers[cnt1].moveTo(x,1000,speed);
+            cnt1++;
+        },2000);
+        handGenerator_medium=setInterval(()=>{
+            let y=getRandomNum(0,835);
+            let speed=getRandomNum(3000,4000);
+            hands.push(new Button(canvas.scene("zsz_left_scene"),-200,y,0,0,11,res.getResource("hand"),null,null,()=>{},()=>{}));
+            hands[cnt2].moveTo(1000,y,speed);
+            cnt2++;
+        },2000);
+    }
+
+    function hardMode(){
+        teacherGenerator_hard=setInterval(()=>{
+            let x=getRandomNum(0,835);
+            let speed=getRandomNum(2000,3000);
+            teachers.push(new Button(canvas.scene("zsz_left_scene"),x,-200,0,0,11,res.getResource("zhoubin"),null,null,()=>{},()=>{}));
+            teachers[cnt1].moveTo(x,1000,speed);
+            cnt1++;
+        },1500);
+        handGenerator_hard=setInterval(()=>{
+            let y=getRandomNum(0,835);
+            let speed=getRandomNum(2000,3000);
+            hands.push(new Button(canvas.scene("zsz_left_scene"),-200,y,0,0,11,res.getResource("hand"),null,null,()=>{},()=>{}));
+            hands[cnt2].moveTo(1000,y,speed);
+            cnt2++;
+        },1500);
+    }
+
+    //TODO 实现手机，判定失败
+
+    var zsz_left_scene_phone=new Button(canvas.scene("zsz_left_scene"),383,464,142,244,10,res.getResource("phone"),null,null,()=>{},()=>{});
+    zsz_left_scene_phone.setDraggable(true);
+    zsz_left_scene_phone.setTransparentAlpha(0);
 
     canvas.changeScene("zsz_door_scene");
 }
