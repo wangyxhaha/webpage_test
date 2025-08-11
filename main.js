577d6b04210757ab18affda2ce7aec596b0690ac
wangyxhaha <wyx12345haha@163.com>
2025-08-11 20:26:05 +0800

    test

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ import Dialog from "./dialog.js";
 var canvas=new Canvas("gameCanvas",935,935)
 
 
-import("./qsy/qsy.js")
+import("./lzan/lzan.js")
     .then(module=>{
         module.default.init(canvas);
     });
