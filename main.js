528b9419d6195794238fec30ab05bdd9e1ea01cb
LiuTianZe070119 <liutianze20070119@qq.com>
2025-08-12 11:05:20 +0800

    xyj data update

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ import Dialog from "./dialog.js";
 
 var canvas=new Canvas("gameCanvas",935,935)
 
-import("./zsz/zsz.js")
+import("./xyj/xyj.js")
     .then(module=>{
         module.default.init(canvas);
     });
